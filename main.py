e0a312bdcb3ac0782e3eb0d5fe35dd30d78db7ec
Ikken9 <pierosaucedo3@gmail.com>
2023-07-17 02:53:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,27 @@ from helpers.scan_selector import *
 from helpers.scan_result import *
 
 
-args = argument_parser()
-host = args.host
-check_host_syntax(host)
+def main():
+    parsed_args = argument_parser()
+    host = parsed_args.host
+    check_host_syntax(host)
 
-ports = str(args.ports).strip()
-port_list = ports.split('-')
-print("[*] Checking if target is online...")
-ping(host)
+    ports = str(parsed_args.ports).strip()
+    port_list = ports.split('-')
+    print("[*] Checking if target is online...")
+    ping(host)
 
-print("[*] Scanning Started at " + strftime("%H:%M:%S") + "\n")
-start_clock = datetime.now()
+    print("[*] Scanning Started at " + strftime("%H:%M:%S") + "\n")
+    start_clock = datetime.now()
 
-scan_result = scan_selector(host, port_list, args.mode)
-make_report(scan_result)
+    scan_result = scan_selector(host, port_list, parsed_args.mode)
+    make_report(scan_result)
 
-stop_clock = datetime.now()
-total_time = stop_clock - start_clock
-print("\n[*] Scanning Finished!")
-print("[*] Total Scan Duration: " + str(total_time))
+    stop_clock = datetime.now()
+    total_time = stop_clock - start_clock
+    print("\n[*] Scanning Finished!")
+    print("[*] Total Scan Duration: " + str(total_time))
+
+
+if __name__ == '__main__':
+    main()
